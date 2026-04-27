79e655091ad7c52f44f46d9914fdedd333cd7471
desonyuan <42114557@qq.com>
2026-04-27 09:00:47 +0800

    添加 appid

--- apps/miniapp/config/dev.js
+++ apps/miniapp/config/dev.js
@@ -3,11 +3,7 @@ module.exports = {
     'process.env.TARO_APP_API_BASE_URL': JSON.stringify(
       process.env.TARO_APP_API_BASE_URL ?? 'http://127.0.0.1:3100/api',
     ),
-    'process.env.TARO_APP_SHOWCASE_DATA': JSON.stringify(
-      process.env.TARO_APP_SHOWCASE_DATA ?? 'true',
-    ),
-    'process.env.TARO_APP_MOCK_WECHAT_PAY': JSON.stringify(
-      process.env.TARO_APP_MOCK_WECHAT_PAY ?? 'true',
-    ),
+    'process.env.TARO_APP_SHOWCASE_DATA': JSON.stringify(process.env.TARO_APP_SHOWCASE_DATA ?? 'true'),
+    'process.env.TARO_APP_MOCK_WECHAT_PAY': JSON.stringify(process.env.TARO_APP_MOCK_WECHAT_PAY ?? 'true'),
   },
 };
